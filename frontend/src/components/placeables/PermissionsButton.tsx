36b865437d210515152af3940d0f976f089a38db
Ritvik Vinodkumar <vinodkumar.r@northeastern.edu>
2021-04-11 20:11:02 -0400

    fixed permissions button

--- frontend/src/components/placeables/PermissionsButton.tsx
+++ frontend/src/components/placeables/PermissionsButton.tsx
@@ -1,12 +1,13 @@
-import { Button, Checkbox, FormControl, FormLabel, Input, MenuItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Table, TableCaption, Tbody, Td, Th, Thead, Tr, useDisclosure, useToast } from "@chakra-ui/react";
+import { Button, Checkbox, FormControl, FormLabel, Input, Menu, MenuItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Table, TableCaption, Tbody, Td, Th, Thead, Tr, useDisclosure, useToast } from "@chakra-ui/react";
 import { Typography } from "@material-ui/core";
 import React, { useCallback, useEffect, useState } from "react";
 import Player from "../../classes/Player";
 import { PlayerPermissionSpecification, PlayerUpdateSpecifications } from "../../classes/TownsServiceClient";
 import useCoveyAppState from "../../hooks/useCoveyAppState";
 import useMaybeVideo from "../../hooks/useMaybeVideo";
+import '../../App.css';
 
-export default function PermissionsButton(): JSX.Element {  
+export default function PermissionsButton(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const video = useMaybeVideo()
   const { players, apiClient, currentTownID } = useCoveyAppState();
@@ -69,47 +70,50 @@ export default function PermissionsButton(): JSX.Element {
     
 
 
-return <>
-<MenuItem data-testid='openPermissionsMenuButton' onClick={openPermissions}>
-<Typography variant="body1">Player Permissions</Typography>
-</MenuItem>
-<Modal isOpen={isOpen} onClose={closePermissions}>  
-<ModalOverlay/>
-<ModalContent>
-  <ModalHeader>change players permissions</ModalHeader>
-  <ModalCloseButton/>
-  <form onSubmit={(ev)=>{ev.preventDefault(); processUpdates('submit')}}>
-    <ModalBody pb={6}>
-    <FormControl isRequired>
-        <FormLabel htmlFor="updatePassword">Town Update Password</FormLabel>
-        <Input data-testid="updatePassword" id="updatePassword" placeholder="Password" name="password" type="password" value={roomUpdatePassword} onChange={(e)=>setRoomUpdatePassword(e.target.value)} />
-    </FormControl>
+return (
+<>
+  <Button className="playerPermissionButton" onClick={openPermissions}>
+    <Typography variant="body1">Player Permissions</Typography>
+  </Button>
 
-    <Table>
-      <TableCaption placement="bottom">Players</TableCaption>
-      <Thead><Tr><Th>Player Name</Th><Th>player ID</Th><Th>can place</Th></Tr></Thead>
-      <Tbody>
-          {currentPlayers?.map((player) => (
-          <Tr key={player.id}><Td role='cell'>{player.userName}</Td><Td
-                      role='cell'>{player.id}</Td>
-                      <Td role='cell'>
-                      <Checkbox isChecked={currentPlayersCanPlace?.get(player.id)} onChange={(e) => setPlayersCanPlace(currentPlayersCanPlace?.set(player.id, e.target.checked))} spacing="1rem">Can Place</Checkbox>
-                      </Td>
-          </Tr>
-       ))}
-      </Tbody>
-    </Table>
-    </ModalBody>
+  <Modal isOpen={isOpen} onClose={closePermissions}>  
+  <ModalOverlay/>
+  <ModalContent>
+    <ModalHeader>change players permissions</ModalHeader>
+    <ModalCloseButton/>
+    <form onSubmit={(ev)=>{ev.preventDefault(); processUpdates('submit')}}>
+      <ModalBody pb={6}>
+      <FormControl isRequired>
+          <FormLabel htmlFor="updatePassword">Town Update Password</FormLabel>
+          <Input data-testid="updatePassword" id="updatePassword" placeholder="Password" name="password" type="password" value={roomUpdatePassword} onChange={(e)=>setRoomUpdatePassword(e.target.value)} />
+      </FormControl>
 
-    <ModalFooter>
-      <Button data-testid='updatebutton' colorScheme="blue" mr={3} value="update" name='action2' onClick={()=>processUpdates('sumbit')}>
-        Update
-      </Button>
-      <Button onClick={closePermissions}>Cancel</Button>
-    </ModalFooter>
-  </form>
-</ModalContent>
-</Modal>
+      <Table>
+        <TableCaption placement="bottom">Players</TableCaption>
+        <Thead><Tr><Th>Player Name</Th><Th>player ID</Th><Th>can place</Th></Tr></Thead>
+        <Tbody>
+            {currentPlayers?.map((player) => (
+            <Tr key={player.id}><Td role='cell'>{player.userName}</Td><Td
+                        role='cell'>{player.id}</Td>
+                        <Td role='cell'>
+                        <Checkbox isChecked={currentPlayersCanPlace?.get(player.id)} onChange={(e) => setPlayersCanPlace(currentPlayersCanPlace?.set(player.id, e.target.checked))} spacing="1rem">Can Place</Checkbox>
+                        </Td>
+            </Tr>
+        ))}
+        </Tbody>
+      </Table>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button data-testid='updatebutton' colorScheme="blue" mr={3} value="update" name='action2' onClick={()=>processUpdates('sumbit')}>
+          Update
+        </Button>
+        <Button onClick={closePermissions}>Cancel</Button>
+      </ModalFooter>
+    </form>
+  </ModalContent>
+  </Modal>
 </>
+)
 
 }
